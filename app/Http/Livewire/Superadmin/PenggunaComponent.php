1249b609b0de4bd88810757d65b89deaba311e47
fajardeveloperr <fajar@nusa.net.id>
2022-07-20 17:24:27 +0700

    update-now

--- app/Http/Livewire/Superadmin/PenggunaComponent.php
+++ app/Http/Livewire/Superadmin/PenggunaComponent.php
@@ -11,16 +11,16 @@ class PenggunaComponent extends Component
 
     public function create_pengguna()
     {
-        try{
-            $this->validate([
-                'nama_pengguna_create'=>'required|min:3|max:50',
-                'email_pengguna_create'=>'required|email|unique:users,email',
-                'password_pengguna_create'=>'min:6|required_with:konfirmasi_password_pengguna_create|same:konfirmasi_pasword_pengguna_create',
-                'konfirmasi_password_pengguna_create'=>'min:6',
-                'utype_pengguna_create'=>'required|min:3|max:50',
+        // try{
+        //     $this->validate([
+        //         'nama_pengguna_create'=>'required|min:3|max:50',
+        //         'email_pengguna_create'=>'required|email|unique:users,email',
+        //         'password_pengguna_create'=>'min:6|required_with:konfirmasi_password_pengguna_create|same:konfirmasi_pasword_pengguna_create',
+        //         'konfirmasi_password_pengguna_create'=>'min:6',
+        //         'utype_pengguna_create'=>'required|min:3|max:50',
 
-            ]);
-        }
+        //     ]);
+        // // }
     }
 
     public function render()
